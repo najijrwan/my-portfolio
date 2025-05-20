6286b5297347b3276eade6f00e023c537b953182
najijrwan <najijrwanyt@gmail.com>
2025-05-20 19:54:19 +0300

    ...

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,14 +5,14 @@ const About = () => {
       <h2 className="heading1">About Me</h2>
       <section>
         <p>
-          A passionate Flutterr developer with strong expertise in cross-platform apps, REST APIs, UI/UX, widgets, and state management solutions.
-          Proven track record in delivering cutting-edge solutions, including API integration, third-party libraries, and performance optimization.
-          Adept at debugging to ensure high-quality, responsive apps and An agile collaborator committed to staying current with industry trends.
+          I'm a detail-oriented and growth-driven software engineer with a strong focus on building intuitive, responsive, and modern web applications.
+          I take pride in writing clean, maintainable code and continuously revisiting my work to improve structure, performance, and user experience.
+          Whether it's frontend development, data handling, or user-centric design, I approach each problem with a mix of precision, creativity, and a deep respect for best practices.
         </p>
         <p>
-          If you're seeking a skilled Flutter developer to breathe life into your project and exceed your expectations,
-          I am here to collaborate and create magic together.
-          Reach out, and let's transform your vision into a reality!
+          My portfolio reflects not just what I’ve built, but how I think—every interface, feature, and interaction is the result of intentional design and thoughtful engineering.
+          I’m constantly expanding my skillset, exploring new technologies, and refining my workflow to produce high-quality, scalable solutions. 
+          I believe in building with purpose, learning with discipline, and always pushing for that extra level of polish that turns good work into great work.
         </p>
       </section>
       <h3 className="heading2 relative">
