f610410c7c3c9de096c6108e02143f5244a331ec
najijrwan <najijrwanyt@gmail.com>
2025-06-01 02:16:32 +0300

    ...

--- src/components/timeLineData.js
+++ src/components/timeLineData.js
@@ -3,7 +3,7 @@ const timeLineData1 = [
     name: "Java",
     imgSrc: "src/assets/java.png",
     color: "#F89820",
-    date: "Mar 2023\nOct 2023\nMar 2024\nOct 2024",
+    date: "Mar 2023",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 0.25,
     left: "2.5rem",
@@ -12,7 +12,7 @@ const timeLineData1 = [
     name: "HTML",
     imgSrc: "src/assets/html.png",
     color: "#F66B4E",
-    date: "Mar 2024\nPresent",
+    date: "Mar 2024",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 0.5,
     left: "10rem",
@@ -21,19 +21,18 @@ const timeLineData1 = [
     name: "CSS",
     imgSrc: "src/assets/css.png",
     color: "#45A8F2",
-    date: "Apr 2024\nPresent",
+    date: "Apr 2024",
     bounceAnim: "animate-bounce-2",
     fadeDelay: 0.75,
     left: "17.5rem",
   },
 ];
-
 const timeLineData2 = [
   {
     name: "JavaScript",
     imgSrc: "src/assets/javascript.png",
     color: "#F7DF1E",
-    date: "May 2024\nPresent",
+    date: "May 2024",
     bounceAnim: "animate-bounce-3",
     fadeDelay: 0.25,
     left: "2.5rem",
@@ -42,7 +41,7 @@ const timeLineData2 = [
     name: "PHP",
     imgSrc: "src/assets/php.png",
     color: "#777BB4",
-    date: "May-2024\nOct 2024",
+    date: "May-2024",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 0.5,
     left: "10rem",
@@ -57,34 +56,42 @@ const timeLineData2 = [
     left: "17.5rem",
   },
 ];
-
 const timeLineData3 = [
+  {
+    name: "Python",
+    imgSrc: "src/assets/python.png",
+    color: "#3776AB",
+    date: "Mar 2025",
+    bounceAnim: "animate-bounce-3",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
   {
     name: "Tailwind",
     imgSrc: "src/assets/tailwind-css.png",
     color: "#06B6D4",
-    date: "May 2024\nPresent",
+    date: "May 2025",
     bounceAnim: "animate-bounce-1",
-    fadeDelay: 0.25,
-    left: "2.5rem",
+    fadeDelay: 0.5,
+    left: "10rem",
   },
   {
     name: "React",
     imgSrc: "src/assets/react.png",
     color: "#61DAFB",
-    date: "May 2024\nPending",
+    date: "May 2025",
     bounceAnim: "animate-bounce-2",
-    fadeDelay: 0.5,
-    left: "10rem",
+    fadeDelay: 0.75,
+    left: "17.5rem",
   },
 ];
 
-const timeLineDataMain = [
+const timeLineDataAtSM = [
   {
     name: "Java",
     imgSrc: "src/assets/java.png",
     color: "#F89820",
-    date: "Mar 2023\nOct 2023\nMar 2024\nOct 2024",
+    date: "Mar 2023",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 0.25,
     left: "2.5rem",
@@ -93,7 +100,149 @@ const timeLineDataMain = [
     name: "HTML",
     imgSrc: "src/assets/html.png",
     color: "#F66B4E",
-    date: "Mar 2024\nPresent",
+    date: "Mar 2024",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 0.5,
+    left: "10rem",
+  },
+  {
+    name: "CSS",
+    imgSrc: "src/assets/css.png",
+    color: "#45A8F2",
+    date: "Apr 2024",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 0.75,
+    left: "17.5rem",
+  },
+  {
+    name: "JavaScript",
+    imgSrc: "src/assets/javascript.png",
+    color: "#F7DF1E",
+    date: "May 2024",
+    bounceAnim: "animate-bounce-3",
+    fadeDelay: 1,
+    left: "25rem",
+  },
+];
+
+const timeLineDataAtSM2 = [
+  {
+    name: "PHP",
+    imgSrc: "src/assets/php.png",
+    color: "#777BB4",
+    date: "May-2024",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
+  {
+    name: "C#",
+    imgSrc: "src/assets/csharp.png",
+    color: "#239120",
+    date: "Oct 2024",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 0.5,
+    left: "10rem",
+  },
+  {
+    name: "Python",
+    imgSrc: "src/assets/python.png",
+    color: "#3776AB",
+    date: "Mar 2025",
+    bounceAnim: "animate-bounce-3",
+    fadeDelay: 0.75,
+    left: "17.5rem",
+  },
+  {
+    name: "Tailwind",
+    imgSrc: "src/assets/tailwind-css.png",
+    color: "#06B6D4",
+    date: "May 2024",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 1,
+    left: "25rem",
+  },
+];
+
+const timeLineDataAtSM3 = [
+  {
+    name: "React",
+    imgSrc: "src/assets/react.png",
+    color: "#61DAFB",
+    date: "May 2025",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
+];
+
+const timeLineDataAtMD = [
+  ...timeLineDataAtSM,
+  {
+    name: "PHP",
+    imgSrc: "src/assets/php.png",
+    color: "#777BB4",
+    date: "May-2024",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 1.25,
+    left: "32.5rem",
+  },
+]
+
+const timeLineDataAtMD2 = [
+  {
+    name: "C#",
+    imgSrc: "src/assets/csharp.png",
+    color: "#239120",
+    date: "Oct 2024",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
+  {
+    name: "Python",
+    imgSrc: "src/assets/python.png",
+    color: "#3776AB",
+    date: "Mar 2025",
+    bounceAnim: "animate-bounce-3",
+    fadeDelay: 0.5,
+    left: "10rem",
+  },
+  {
+    name: "Tailwind",
+    imgSrc: "src/assets/tailwind-css.png",
+    color: "#06B6D4",
+    date: "May 2024",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 0.75,
+    left: "17.5rem",
+  },
+  {
+    name: "React",
+    imgSrc: "src/assets/react.png",
+    color: "#61DAFB",
+    date: "May 2025",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 1,
+    left: "25rem",
+  }
+];
+
+const timeLineDataAtLG = [
+  {
+    name: "Java",
+    imgSrc: "src/assets/java.png",
+    color: "#F89820",
+    date: "Mar 2023",
+    bounceAnim: "animate-bounce-1",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
+  {
+    name: "HTML",
+    imgSrc: "src/assets/html.png",
+    color: "#F66B4E",
+    date: "Mar 2024",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 0.5,
     left: "9rem",
@@ -102,7 +251,7 @@ const timeLineDataMain = [
     name: "CSS",
     imgSrc: "src/assets/css.png",
     color: "#45A8F2",
-    date: "Apr 2024\nPresent",
+    date: "Apr 2024",
     bounceAnim: "animate-bounce-2",
     fadeDelay: 0.75,
     left: "15.5rem",
@@ -111,7 +260,7 @@ const timeLineDataMain = [
     name: "JavaScript",
     imgSrc: "src/assets/javascript.png",
     color: "#F7DF1E",
-    date: "May 2024\nPresent",
+    date: "May 2024",
     bounceAnim: "animate-bounce-3",
     fadeDelay: 1,
     left: "22rem",
@@ -120,7 +269,7 @@ const timeLineDataMain = [
     name: "PHP",
     imgSrc: "src/assets/php.png",
     color: "#777BB4",
-    date: "May-2024\nOct 2024",
+    date: "May-2024",
     bounceAnim: "animate-bounce-1",
     fadeDelay: 1.25,
     left: "28.5rem",
@@ -135,23 +284,42 @@ const timeLineDataMain = [
     left: "35rem",
   },
   {
-    name: "Tailwind",
-    imgSrc: "src/assets/tailwind-css.png",
-    color: "#06B6D4",
-    date: "May 2024\nPresent",
-    bounceAnim: "animate-bounce-1",
+    name: "Python",
+    imgSrc: "src/assets/python.png",
+    color: "#3776AB",
+    date: "Mar 2025",
+    bounceAnim: "animate-bounce-3",
     fadeDelay: 1.75,
     left: "41.5rem",
   },
   {
-    name: "React",
-    imgSrc: "src/assets/react.png",
-    color: "#61DAFB",
-    date: "May 2024\nPending",
-    bounceAnim: "animate-bounce-2",
+    name: "Tailwind",
+    imgSrc: "src/assets/tailwind-css.png",
+    color: "#06B6D4",
+    date: "May 2024",
+    bounceAnim: "animate-bounce-1",
     fadeDelay: 2,
     left: "48rem",
   },
 ];
 
-export { timeLineData1, timeLineData2, timeLineData3, timeLineDataMain };
+const timeLineDataAtLG2 = [
+  {
+    name: "React",
+    imgSrc: "src/assets/react.png",
+    color: "#61DAFB",
+    date: "May 2025",
+    bounceAnim: "animate-bounce-2",
+    fadeDelay: 0.25,
+    left: "2.5rem",
+  },
+]
+
+
+
+export {
+  timeLineData1, timeLineData2, timeLineData3,
+  timeLineDataAtSM, timeLineDataAtSM2, timeLineDataAtSM3,
+  timeLineDataAtMD, timeLineDataAtMD2,
+  timeLineDataAtLG, timeLineDataAtLG2
+};
--- src/pages/Career1.jsx
+++ src/pages/Career1.jsx
@@ -1,6 +1,11 @@
 import Navbar from "../components/Navbar";
 import { h2Ele } from './About.jsx';
-import { timeLineData1, timeLineData2, timeLineData3, timeLineDataMain } from "../components/timeLineData";
+import {
+  timeLineData1, timeLineData2, timeLineData3,
+  timeLineDataAtSM, timeLineDataAtSM2, timeLineDataAtSM3,
+  timeLineDataAtMD, timeLineDataAtMD2,
+  timeLineDataAtLG, timeLineDataAtLG2
+} from "../components/timeLineData";
 import TimelineItem from "../components/TimelineItem";
 
 // Career.jsx
@@ -11,24 +16,13 @@ const divEle1 = `
 `;
 const Career = () => {
   return (
-    <article>
+    <article className="">
       <Navbar />
       <div className="p-[20px] sm:p-[30px]">
         <h2 className={h2Ele}>Journey</h2>
-        <div className="relative w-full flex-center-col gap-[50px] lg:gap-[100px]">
+        <div className="relative w-full flex-center-col gap-[150px] mb-15 mt-30">
           {/* Timeline Section */}
-          <section className="relative w-full mt-25 justify-center hidden lg:flex">
-            {/* Arrow Line */}
-            <div className="w-full max-w-[350px] h-0.5 relative lg:max-w-full">
-              <div className={divEle1}>
-                {timeLineDataMain.map((item, index) => (
-                  <TimelineItem key={item.name} item={item} index={index} />
-                ))}
-              </div>
-            </div>
-          </section>
-          {/* Timeline Section */}
-          <section className="relative w-full mt-25 flex justify-center lg:hidden">
+          <section className="relative w-full flex justify-center sm:hidden lg:hidden">
             {/* Arrow Line */}
             <div className="w-full max-w-[350px] h-0.5 relative">
               <div className={divEle1}>
@@ -39,7 +33,7 @@ const Career = () => {
             </div>
           </section>
           {/* Timeline Section */}
-          <section className="relative w-full mt-40 flex justify-center lg:hidden">
+          <section className="relative w-full flex justify-center sm:hidden lg:hidden">
             {/* Arrow Line */}
             <div className="w-full max-w-[350px] h-0.5 relative">
               <div className={divEle1}>
@@ -50,11 +44,88 @@ const Career = () => {
             </div>
           </section>
           {/* Timeline Section */}
-          <section className="relative w-full mt-30 flex justify-center lg:hidden">
+          <section className="relative w-full flex justify-center sm:hidden lg:hidden">
             {/* Arrow Line */}
             <div className="w-full max-w-[350px] h-0.5 relative">
               <div className={divEle1}>
                 {timeLineData3.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at small*/}
+          <section className="relative w-full hidden sm:block md:hidden lg:hidden">
+            {/* Arrow Line */}
+            <div className="w-full h-0.5 relative">
+              <div className={divEle1}>
+                {timeLineDataAtSM.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at small*/}
+          <section className="relative w-full hidden sm:block md:hidden lg:hidden">
+            {/* Arrow Line */}
+            <div className="w-full h-0.5 relative">
+              <div className={divEle1}>
+                {timeLineDataAtSM2.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at small*/}
+          <section className="relative w-full hidden sm:block md:hidden lg:hidden">
+            {/* Arrow Line */}
+            <div className="w-full h-0.5 relative">
+              <div className={divEle1}>
+                {timeLineDataAtSM3.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at medium*/}
+          <section className="relative w-full hidden md:block lg:hidden">
+            {/* Arrow Line */}
+            <div className="w-full h-0.5 relative">
+              <div className={divEle1}>
+                {timeLineDataAtMD.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at medium*/}
+          <section className="relative w-full hidden md:block lg:hidden">
+            {/* Arrow Line */}
+            <div className="w-full h-0.5 relative">
+              <div className={divEle1}>
+                {timeLineDataAtMD2.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at large*/}
+          <section className="relative w-full hidden lg:flex lg:justify-center">
+            {/* Arrow Line */}
+            <div className="w-full max-w-[350px] h-0.5 relative lg:max-w-full">
+              <div className={divEle1}>
+                {timeLineDataAtLG.map((item, index) => (
+                  <TimelineItem key={item.name} item={item} index={index} />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/* Timeline Section at large*/}
+          <section className="relative w-full hidden lg:flex lg:justify-center">
+            {/* Arrow Line */}
+            <div className="w-full max-w-[350px] h-0.5 relative lg:max-w-full">
+              <div className={divEle1}>
+                {timeLineDataAtLG2.map((item, index) => (
                   <TimelineItem key={item.name} item={item} index={index} />
                 ))}
               </div>
