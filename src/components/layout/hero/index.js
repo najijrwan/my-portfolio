3bc2dd33f705da2b152fe35b9aea88a83970de08
najijrwan <najijrwanyt@gmail.com>
2026-02-13 19:26:41 +0200

    .

---
+++ src/components/layout/hero/index.js
@@ -0,0 +1,10 @@
+export { default as Hero } from './Hero'
+
+// Hero info block
+export { default as Avatar } from './Avatar'
+export { default as NameAndTitle } from './NameAndTitle'
+export { default as ShowContactsButton } from './ShowContactsButton'
+
+// Hero Contacts and Social Links
+export { default as Contacts } from './Contacts'
+export { default as SocialLinks } from './SocialLinks';
