f53c106313ef2f66f5316753e732084c39d8c7bf
najijrwan <najijrwanyt@gmail.com>
2026-02-15 09:19:01 +0200

    refactor(hero/heroheader): swapped inline svg with ion-icon

--- src/components/layout/hero/HeroHeader.jsx
+++ src/components/layout/hero/HeroHeader.jsx
@@ -62,23 +62,13 @@ export const ShowContactsButton = ({ onClick, isExpanded }) => (
         focus:bg-gradient-yellow focus:outline-amber-200 focus:before:bg-gradient-yellow-2"
     >
         <span className="hidden sm:block sm:text-[12px] tracking-wider">Show Contacts</span>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+        <ion-icon
+            name="chevron-down-outline"
             className={`
-                lucide lucide-chevron-down
                 block sm:hidden 
                 transition-transform duration-300 ${isExpanded ? 'rotate-180' : ''} `}
         >
-            <path d="m6 9 6 6 6-6" />
-        </svg>
+        </ion-icon>
     </button>
 )
 
