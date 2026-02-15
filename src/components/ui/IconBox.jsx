d548a0afa99993392d25baf300c9a1e5fd5a9dd2
najijrwan <najijrwanyt@gmail.com>
2026-02-15 21:12:03 +0200

    refactor(ui/IconBox): let the icon itself decide it's styling

--- src/components/ui/IconBox.jsx
+++ src/components/ui/IconBox.jsx
@@ -1,15 +1,18 @@
 const IconBox = ({ iconName }) => (
     <div
         className="
-            relative z-10 
+            relative z-10
             size-[30px] sm:size-[48px]
             flex-center
-            text-highlight text-[16px] sm:text-[18px] stroke-3
             bg-gradient-onyx rounded-[8px] sm:rounded-[12px] shadow-1 
-            before:content-[''] before:absolute before:inset-px before:-z-1
+            before:content-[''] before:absolute before:inset-px before:-z-10
             before:bg-eerie-1 before:rounded-[inherit]"
     >
-        <ion-icon name={iconName}></ion-icon>
+        <ion-icon
+            name={iconName}
+            className='text-highlight size-4 sm:size-4.5 [--ionicon-stroke-width:40px]'
+        >
+        </ion-icon>
     </div>
 )
 
