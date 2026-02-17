2b37dae42e08819ef9565f917e8df8b88ee0b8f0
najijrwan <najijrwanyt@gmail.com>
2026-02-17 08:46:53 +0200

    style(layout): increase Navbar spacing and text size

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => (
     <nav
         className={`
             w-full lg:w-max p-3 lg:px-10 lg:py-5 lg:ml-auto
-            flex-start-col flex-wrap gap-[15px] sm:gap-[35px] md:gap-[55px]
-            text-[12px] sm:text-[15px] md:text-[18px] lg:text-[15px] font-[600]
+            flex-start-col flex-wrap gap-[25px] sm:gap-[40px] md:gap-[55px]
+            text-[11px] sm:text-[15px] md:text-[18px] lg:text-[15px] font-[600]
             bg-[#2b2b2cbf] backdrop-blur-[10px] border-b border-b-jet rounded-tl-[20px]
             sm:rounded-tl-[20px] rounded-tr-[20px] sm:rounded-tr-[20px] lg:rounded-tr-[20px] lg:rounded-bl-[20px] 
             lg:rounded-none shadow-2 lg:shadow-none overflow-hidden
