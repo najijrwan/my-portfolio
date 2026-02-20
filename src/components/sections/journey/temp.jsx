4b6918abd913536a7392f5dcfbb094443a9dacba
najijrwan <najijrwanyt@gmail.com>
2026-02-20 22:29:30 +0200

    refactor(journey): enhance layout and styling of Temp component

--- src/components/sections/journey/temp.jsx
+++ src/components/sections/journey/temp.jsx
@@ -1,64 +1,18 @@
 
 
 const Temp = () => (
-    <section className="flex flex-wrap gap-10">
-        <section
-            className="flex items-end"
-        >
-            <div
-                className="flex items-center"
-            >
-                <div className="w-7.5 h-px bg-jet"></div>
-                <div
-                    className="
-                    w-15 h-8
-                    flex flex-col items-center justify-center
-                    text-xs text-black
-                    bg-yellow-crayola"
-                >
-                    <span>Mar</span>
-                    <span>2023</span>
-                </div>
-            </div>
-
-            <div
-                className="pb-4 flex flex-col items-center justify-center"
-            >
-                <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
-                    <img src="images/java.png" alt="" className="size-10" />
-                </div>
-                <div className="w-px h-5 bg-jet"></div>
-                <div className="w-20 h-px bg-jet"></div>
-            </div>
-
-            <div
-                className="flex items-center"
-            >
-                <div
-                    className="
-                    w-15 h-8
-                    flex flex-col items-center justify-center
-                    text-xs text-black
-                    bg-yellow-crayola"
-                >
-                    <span>Mar</span>
-                    <span>2023</span>
-                </div>
-            </div>
-        </section>
-
-
-        <section className="w-full flex items-center">
+    <section className="flex flex-wrap">
+        <section className="w-fit flex items-center">
             <div
                 className="flex items-center"
             >
                 <div className="w-5 h-px bg-jet"></div>
                 <div
                     className="
-                    w-15 h-8
+                    w-10 h-8
                     flex flex-col items-center justify-center
                     text-xs text-black
-                    bg-yellow-crayola rounded-sm"
+                    bg-yellow-crayola"
                 >
                     <span>Mar</span>
                     <span>2023</span>
@@ -66,7 +20,97 @@ const Temp = () => (
             </div>
 
             <div
-                className=""
+                className="h-[125px]"
+            >
+                <div className="w-fit px-2.5 flex gap-2.5">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
+                            <img src="images/java.png" alt="" className="size-8" />
+                        </div>
+                        <div className="w-px h-5 bg-jet"></div>
+                    </div>
+                </div>
+
+                <div className="h-px bg-jet"></div>
+            </div>
+
+            <div className="relative px-2 py-3 border border-jet rounded-lg">
+                <div
+                    className="
+                        absolute top-0 left-1/2 -translate-1/2
+                        px-1
+                        text-[10px] text-yellow-crayola
+                        border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+                <div
+                    className="
+                        px-1
+                        text-[10px] text-yellow-crayola
+                       border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+                <div
+                    className="
+                        absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2
+                        px-1
+                        text-[10px] text-yellow-crayola
+                       border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+            </div>
+            <div
+                className="flex items-center"
+            >
+                <div className="w-5 h-px bg-jet"></div>
+                <div
+                    className="
+                    w-10 h-8
+                    flex flex-col items-center justify-center
+                    text-xs text-black
+                    bg-yellow-crayola"
+                >
+                    <span>May</span>
+                    <span>2023</span>
+                </div>
+                <div
+                    className="
+                        w-0 h-0 z-20
+                        border-t-[16px] border-t-transparent
+                        border-b-[16px] border-b-transparent
+                        border-l-[8px] border-l-yellow-crayola"
+                >
+                </div>
+            </div>
+        </section>
+
+        <section className="w-fit flex items-center">
+            <div className="flex items-center">
+                <div
+                    className="
+                        relative
+                        w-10 h-8
+                        flex flex-col items-center justify-center
+                        text-xs text-black
+                        bg-amber-600
+                        before:content-['']
+                        before:absolute before:left-0 before:-translate-x-full before:top-0 before:z-10
+                        before:w-2 before:h-8
+                        before:bg-amber-600
+                        "
+                >
+                    <span>Mar</span>
+                    <span>2023</span>
+
+                </div>
+            </div>
+
+
+            <div
+                className="h-[150px] flex flex-col justify-center"
             >
                 <div className="w-fit px-2.5 flex gap-2.5">
                     <div className="flex flex-col items-center justify-center">
@@ -83,15 +127,9 @@ const Temp = () => (
                     </div>
                 </div>
 
-                <div className="h-px bg-jet"></div>
+                <div className="w-full h-px bg-jet"></div>
 
                 <div className="w-fit pl-[36px] pr-2.5 flex gap-2.5">
-                    <div className="flex flex-col-reverse items-center justify-center">
-                        <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
-                            <img src="images/java.png" alt="" className="size-8" />
-                        </div>
-                        <div className="w-px h-5 bg-jet"></div>
-                    </div>
                     <div className="flex flex-col-reverse items-center justify-center">
                         <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
                             <img src="images/java.png" alt="" className="size-8" />
@@ -101,13 +139,13 @@ const Temp = () => (
                 </div>
             </div>
 
-            <div className="relative px-2 py-3 border bg-[#2b2b2cbf]  border-jet rounded-xl">
+            <div className="relative px-2 py-3 border border-jet rounded-lg">
                 <div
                     className="
                         absolute top-0 left-1/2 -translate-1/2
                         px-1
                         text-[10px] text-yellow-crayola
-                        bg-amber-600 rounded-sm"
+                        border border-jet bg-eerie-2 rounded-lg"
                 >
                     Fitnesso
                 </div>
@@ -115,7 +153,7 @@ const Temp = () => (
                     className="
                         px-1
                         text-[10px] text-yellow-crayola
-                        bg-amber-600 rounded-sm"
+                       border border-jet bg-eerie-2 rounded-lg"
                 >
                     Fitnesso
                 </div>
@@ -124,9 +162,128 @@ const Temp = () => (
                         absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2
                         px-1
                         text-[10px] text-yellow-crayola
-                        bg-amber-600 rounded-sm"
+                       border border-jet bg-eerie-2 rounded-lg"
                 >
                     Fitnesso
+                </div>
+            </div>
+            <div
+                className="flex items-center"
+            >
+                <div className="w-5 h-px bg-jet"></div>
+                <div
+                    className="
+                    w-10 h-8
+                    flex flex-col items-center justify-center
+                    text-xs text-black
+                    bg-amber-600"
+                >
+                    <span>Mar</span>
+                    <span>2023</span>
+                </div>
+                <div
+                    className="
+                        w-0 h-0 z-20
+                        border-t-[16px] border-t-transparent
+                        border-b-[16px] border-b-transparent
+                        border-l-[8px] border-l-amber-600"
+                >
+                </div>
+            </div>
+        </section>
+        <section className="w-fit flex items-center">
+            <div className="flex items-center">
+                <div
+                    className="
+                        relative
+                        w-10 h-8
+                        flex flex-col items-center justify-center
+                        text-xs text-black
+                        bg-blue-400
+                        before:content-['']
+                        before:absolute before:left-0 before:-translate-x-full before:top-0 before:z-10
+                        before:w-2 before:h-8
+                        before:bg-blue-400
+                        "
+                >
+                    <span>Mar</span>
+                    <span>2023</span>
+
+                </div>
+            </div>
+
+
+            <div
+                className="h-[150px] flex flex-col justify-center"
+            >
+                <div className="w-fit px-2.5 flex gap-2.5">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
+                            <img src="images/java.png" alt="" className="size-8" />
+                        </div>
+                        <div className="w-px h-5 bg-jet"></div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
+                            <img src="images/java.png" alt="" className="size-8" />
+                        </div>
+                        <div className="w-px h-5 bg-jet"></div>
+                    </div>
+                </div>
+
+                <div className="w-full h-px bg-jet"></div>
+
+                <div className="w-fit pl-[36px] pr-2.5 flex gap-2.5">
+                    <div className="flex flex-col-reverse items-center justify-center">
+                        <div className="bg-[#2b2b2cbf] border border-jet p-1 rounded-2xl">
+                            <img src="images/java.png" alt="" className="size-8" />
+                        </div>
+                        <div className="w-px h-5 bg-jet"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative px-2 py-3 border border-jet rounded-lg">
+                <div
+                    className="
+                        absolute top-0 left-1/2 -translate-1/2
+                        px-1
+                        text-[10px] text-yellow-crayola
+                        border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+                <div
+                    className="
+                        px-1
+                        text-[10px] text-yellow-crayola
+                       border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+                <div
+                    className="
+                        absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2
+                        px-1
+                        text-[10px] text-yellow-crayola
+                       border border-jet bg-eerie-2 rounded-lg"
+                >
+                    Fitnesso
+                </div>
+            </div>
+            <div
+                className="flex items-center"
+            >
+                <div className="w-5 h-px bg-jet"></div>
+                <div
+                    className="
+                    w-10 h-8
+                    flex flex-col items-center justify-center
+                    text-xs text-black
+                    bg-blue-400"
+                >
+                    <span>Mar</span>
+                    <span>2023</span>
                 </div>
             </div>
         </section>
