a5e5aa1213b8dd9e1641ebeb33b88707e07c0339
najijrwan <najijrwanyt@gmail.com>
2026-02-21 22:55:05 +0200

    feat(journey): add ArrowHead component to display month and year

---
+++ src/components/sections/journey/ArrowHead.jsx
@@ -0,0 +1,26 @@
+export const ArrowHead = ({ month, year, }) => (
+    <div
+        className="flex items-center"
+    >
+
+        <div
+            className="
+                w-10 h-5
+                flex-center flex-col
+                text-[10px]/2.5 text-black tracking-wider
+                bg-yellow-crayola
+                "
+        >
+            <span>{month}</span>
+            <span>{year}</span>
+        </div>
+        <div
+            className="
+                w-0 h-0 z-20
+                border-t-[10px] border-t-transparent
+                border-b-[10px] border-b-transparent
+                border-l-[12px] border-l-yellow-crayola
+                "
+        />
+    </div>
+)
