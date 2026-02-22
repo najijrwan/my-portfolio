caa81179a96074f8f0e9d32e5637989043bf0105
najijrwan <najijrwanyt@gmail.com>
2026-02-22 22:39:24 +0200

    refactor(journey): change ArrowHead code lines

--- src/components/sections/journey/ArrowHead.jsx
+++ src/components/sections/journey/ArrowHead.jsx
@@ -3,22 +3,22 @@ const ArrowHead = ({ month, year, color }) => {
         <div className="flex items-center">
             <div
                 className={`
-          w-12 h-6
-          flex-center flex-col
-          text-[10px]/2.5 text-black tracking-wider font-bold
-          ${color.bg}
-        `}
+                    w-12 h-6
+                    flex-center flex-col
+                    text-[10px]/2.5 text-black tracking-wider font-bold
+                    ${color.bg}
+                `}
             >
                 <span>{month}</span>
                 <span>{year}</span>
             </div>
             <div
                 className={`
-          w-0 h-0 z-20
-          border-t-[12px] border-t-transparent
-          border-b-[12px] border-b-transparent
-          border-l-[12px] ${color.border}
-        `}
+                    w-0 h-0
+                    border-t-[12px] border-t-transparent
+                    border-b-[12px] border-b-transparent
+                    border-l-[12px] ${color.border}
+                `}
             />
         </div>
     );
