15c555598388a202b4df040db20b8d4ec6a40c89
najijrwan <najijrwanyt@gmail.com>
2026-02-22 22:41:31 +0200

    style(journey): change Timeline layout at lg breakpoint

--- src/components/sections/journey/Timeline.jsx
+++ src/components/sections/journey/Timeline.jsx
@@ -1,34 +1,65 @@
+import { VariousActionsButton } from '@ui'
 import { timelineArrows } from '@data';
 import ArrowTail from './ArrowTail';
 import ArrowHead from './ArrowHead';
 import { ArrowBodySkills, ArrowBodyProjects } from './ArrowBody';
 
 const COLORS = [
-  { bg: "bg-blue-300", border: "border-blue-300" },
-  { bg: "bg-green-300", border: "border-green-300" },
-  { bg: "bg-red-300", border: "border-red-300" },
-  { bg: "bg-cyan-300", border: "border-cyan-300" },
-  { bg: "bg-purple-300", border: "border-purple-300" },
-  { bg: "bg-lime-300", border: "border-lime-300" },
-  { bg: "bg-orange-300", border: "border-orange-300" },
+    { bg: "bg-blue-300", border: "border-blue-300" },
+    { bg: "bg-green-300", border: "border-green-300" },
+    { bg: "bg-red-300", border: "border-red-300" },
+    { bg: "bg-cyan-300", border: "border-cyan-300" },
+    { bg: "bg-purple-300", border: "border-purple-300" },
+    { bg: "bg-lime-300", border: "border-lime-300" },
+    { bg: "bg-orange-300", border: "border-orange-300" },
 ];
 
 const Timeline = () => (
-    <section className="pb-5 flex overflow-x-scroll has-scrollbar">
-        {timelineArrows.map((arrow, i) => {
-            const color = COLORS[i % COLORS.length];
+    <section className="relative flex flex-col">
+        {/* <div className='ml-auto flex gap-2'>
+            <VariousActionsButton
+                variant='v2'
+                iconName='chevron-back-outline'
+            />
+            <VariousActionsButton
+                variant='v2'
+                iconName='chevron-forward-outline'
+            />
+        </div> */}
 
-            return (
-                <section key={i} className="flex items-center">
-                    <ArrowTail color={color} month={arrow.startDateMonth} year={arrow.startDateYear} />
+        {/* <div
+            className="
+                absolute top-0 left-0 z-20
+                h-full w-20
+                bg-gradient-to-r from-[hsl(240,2%,12%)] to-transparent
+                pointer-events-none
+                "
+        /> */}
 
-                    {arrow.skills?.length > 0 && <ArrowBodySkills skills={arrow.skills} />}
-                    {arrow.projects?.length > 0 && <ArrowBodyProjects projects={arrow.projects} />}
+        <section className="pb-5 flex overflow-x-scroll has-scrollbar lg:flex-wrap">
+            {timelineArrows.map((arrow, i) => {
+                const color = COLORS[i % COLORS.length];
 
-                    <ArrowHead color={color} month={arrow.endDateMonth} year={arrow.endDateYear} />
-                </section>
-            );
-        })}
+                return (
+                    <section key={i} className="flex items-center lg:py-5">
+                        <ArrowTail color={color} month={arrow.startDateMonth} year={arrow.startDateYear} />
+
+                        {arrow.skills?.length > 0 && <ArrowBodySkills skills={arrow.skills} />}
+                        {arrow.projects?.length > 0 && <ArrowBodyProjects projects={arrow.projects} />}
+
+                        <ArrowHead color={color} month={arrow.endDateMonth} year={arrow.endDateYear} />
+                    </section>
+                );
+            })}
+        </section>
+
+        {/* <div
+            className="
+                absolute top-0 right-0 z-20
+                w-20 h-full
+                bg-gradient-to-l from-[hsl(240,2%,12%)] to-transparent
+                pointer-events-none"
+        /> */}
     </section>
 
 );
