d71996f3df9b4ab9d612908c4d2761e47a847087
najijrwan <najijrwanyt@gmail.com>
2026-02-22 21:11:47 +0200

    style(journey): increase arrow's head and tail height

--- src/components/sections/journey/ArrowHead.jsx
+++ src/components/sections/journey/ArrowHead.jsx
@@ -3,7 +3,7 @@ const ArrowHead = ({ month, year, color }) => {
         <div className="flex items-center">
             <div
                 className={`
-          w-12 h-5
+          w-12 h-6
           flex-center flex-col
           text-[10px]/2.5 text-black tracking-wider font-bold
           ${color.bg}
@@ -15,8 +15,8 @@ const ArrowHead = ({ month, year, color }) => {
             <div
                 className={`
           w-0 h-0 z-20
-          border-t-[10px] border-t-transparent
-          border-b-[10px] border-b-transparent
+          border-t-[12px] border-t-transparent
+          border-b-[12px] border-b-transparent
           border-l-[12px] ${color.border}
         `}
             />
--- src/components/sections/journey/ArrowTail.jsx
+++ src/components/sections/journey/ArrowTail.jsx
@@ -2,11 +2,11 @@ const ArrowTail = ({ month, year, color }) => {
   return (
     <div
       className={`
-        w-15 h-5
-        flex-center flex-col gap-px
+        w-15 h-6
+        flex-center flex-col gap-1
         text-[10px]/2 text-black tracking-wider font-bold
-        border-t-[10px] ${color.border}
-        border-b-[10px] ${color.border}
+        border-t-[12px] ${color.border}
+        border-b-[12px] ${color.border}
         border-l-[12px] border-l-transparent
       `}
     >
