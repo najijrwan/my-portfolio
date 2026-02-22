ce479e62295a3228f8e0107e2ae0b0b891036f2b
najijrwan <najijrwanyt@gmail.com>
2026-02-22 20:26:27 +0200

    style(ui): redce skillLollipop image size

--- src/components/ui/SkillLollipop.jsx
+++ src/components/ui/SkillLollipop.jsx
@@ -1,7 +1,7 @@
 const SkillLollipop = ({ src, lan, reversed = false, }) => (
     <div className={`flex-center ${reversed ? 'flex-col-reverse' : 'flex-col'}`}>
         <div className="forward-box p-1 rounded-2xl cursor-pointer">
-            <img src={src} alt={lan} className="size-12" />
+            <img src={src} alt={lan} className="size-11" />
         </div>
         <div className="w-px h-5 bg-jet"></div>
     </div>
