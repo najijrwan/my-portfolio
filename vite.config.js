b6a249909ebcede400fffdda2d56c6dab3703af1
najijrwan <najijrwanyt@gmail.com>
2026-02-25 23:49:42 +0200

    feat: add utils path alias

--- vite.config.js
+++ vite.config.js
@@ -32,6 +32,8 @@ export default defineConfig({
       '@hooks': path.resolve(__dirname, './src/hooks'),
 
       '@pages': path.resolve(__dirname, './src/pages'),
+
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 })
