901059d41681d9d22b8b07090e929c5f05f00388
najijrwan <najijrwanyt@gmail.com>
2026-02-25 23:54:27 +0200

    feat(journey): import useMemo hook to store all the projects in an object const using mergeTimelineProjects and map them in a section using the component ProjectDetails

--- src/components/sections/journey/timeline-projects-details/TimelineProjectsDetails.jsx
+++ src/components/sections/journey/timeline-projects-details/TimelineProjectsDetails.jsx
@@ -1,9 +1,22 @@
+import { useMemo } from "react";
+import { mergeTimelineProjects } from "@utils"
 import { SectionTitle } from '@about'
+import ProjectDetails from './ProjectDetails'
 
-const TimelineProjectsDetails = () => (
-    <section>
-        <SectionTitle title="Projects Details" />
-    </section>
-)
+const TimelineProjectsDetails = () => {
+    const MERGED_PROJECTS = useMemo(() => mergeTimelineProjects({ dedupe: true }), []);
+
+    return (
+        <section>
+            <SectionTitle title="Projects Details" />
+
+            <section className="pb-5 flex flex-wrap gap-4 overflow-x-auto has-scrollbar">
+                {MERGED_PROJECTS.map((project) => (
+                    <ProjectDetails key={project.ref ?? project.title} project={project} />
+                ))}
+            </section>
+        </section>
+    )
+}
 
 export default TimelineProjectsDetails;
