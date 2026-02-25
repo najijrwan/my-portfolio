624789657fa7e998887d350c93d80525578310e3
najijrwan <najijrwanyt@gmail.com>
2026-02-25 22:52:33 +0200

    feat(journey): create a folder timeline-projects-details and a component TimelineProjectsDetails

---
+++ src/components/sections/journey/timeline-projects-details/TimelineProjectsDetails.jsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from '@about'
+
+const TimelineProjectsDetails = () => (
+    <section>
+        <SectionTitle title="Projects Details" />
+    </section>
+)
+
+export default TimelineProjectsDetails;
