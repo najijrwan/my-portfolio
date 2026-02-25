5d05f93ae3c2c8ee840692e4f7848976939dcdf4
najijrwan <najijrwanyt@gmail.com>
2026-02-25 22:48:55 +0200

    feat(about): export SectionTitle

--- src/components/sections/about/index.js
+++ src/components/sections/about/index.js
@@ -1,3 +1,5 @@
 export { default as AboutMe } from './AboutMe'
+export { default as SectionContainer } from './SectionContainer'
+export { SectionTitle } from './SectionContainer'
 export { default as Roles } from './Roles'
 export { default as Skills } from './Skills'
