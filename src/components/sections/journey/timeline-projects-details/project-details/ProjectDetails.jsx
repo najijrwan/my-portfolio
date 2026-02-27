7f48afd6b1c33eaf39ce632d34bb319ebb066cc8
najijrwan <najijrwanyt@gmail.com>
2026-02-27 22:02:20 +0200

    refactor(journey): create a new folder and move ProjectDetails to it

---
+++ src/components/sections/journey/timeline-projects-details/project-details/ProjectDetails.jsx
@@ -0,0 +1,59 @@
+import { useExtend } from '@hooks'
+import ProjectFields from './ProjectFields'
+
+const ProjectDetails = ({ project }) => {
+    const { isExtended, handleClick } = useExtend();
+
+    return (
+        <section
+            className={`
+                relative
+                flex flex-col gap-4
+                bg-[#2b2b2c] border border-jet rounded-xl
+                animate-height
+            `}
+        >
+            <ProjectDetailsHeader label={project.label ?? project.ref} />
+
+            <ProjectFields project={project} isExtended={isExtended} />
+
+            <ExtendFieldsBtn onClick={handleClick} isExtended={isExtended} />
+        </section>
+    );
+};
+
+export default ProjectDetails;
+
+
+const ProjectDetailsHeader = ({ label }) => (
+    <header
+        className='
+            absolute top-0 left-1/2 -translate-1/2
+            min-w-max px-2 py-1.5
+            text-[10px] sm:text-[12px] text-white text-center tracking-wider uppercase
+            bg-jet rounded-lg
+        '
+    >
+        {label}
+    </header>
+)
+
+const ExtendFieldsBtn = ({ onClick, isExtended }) => (
+    <button
+        onClick={onClick}
+        className="
+            w-full py-3
+            flex-center bg-jet
+            text-yellow-crayola rounded-b-[inherit]
+            hover:opacity-50
+            transition-opacity duration-150 ease-in-out"
+    >
+        <ion-icon
+            src="icons/chevrons-down.svg"
+            className={`
+                size-4 visible! transform transition-transform duration-150 ease-in-out
+                ${isExtended ? 'rotate-180' : 'rotate-0'}
+            `}
+        />
+    </button>
+)
