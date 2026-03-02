c8fd469ec28b647eb5939bfbb89a2f308d7e1d20
najijrwan <najijrwanyt@gmail.com>
2026-03-02 23:38:07 +0200

    refactor(hooks): group projects filters related hooks into a new folder and update the imports and exports

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,4 +3,4 @@ export { useSmartTooltipPosition } from './useSmartTooltipPosition';
 export { useExtend } from './useExtend';
 export { useBreakpoint } from './useBreakpoint';
 export { useContactFormConfig } from './useContactFormConfig';
-export { useProjectsFilters } from './useProjectsFilters';
+export { useProjectsFilters } from './projects-filters/useProjectsFilters';
