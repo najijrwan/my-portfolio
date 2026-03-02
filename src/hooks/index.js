23c46fe71540b91e69db931d6c6e0560ca2c722d
najijrwan <najijrwanyt@gmail.com>
2026-03-02 21:56:24 +0200

    feat(hooks): export as named function useContactFormConfig.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { useScrollFade } from './useScrollFade';
 export { useSmartTooltipPosition } from './useSmartTooltipPosition';
 export { useExtend } from './useExtend';
 export { useBreakpoint } from './useBreakpoint';
+export { useContactFormConfig } from './useContactFormConfig';
