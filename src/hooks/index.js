0cdca0656910cdef4b729c469365293054e14046
najijrwan <najijrwanyt@gmail.com>
2026-03-02 23:35:22 +0200

    feat(hooks): export useProjectsFilters

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,4 @@ export { useSmartTooltipPosition } from './useSmartTooltipPosition';
 export { useExtend } from './useExtend';
 export { useBreakpoint } from './useBreakpoint';
 export { useContactFormConfig } from './useContactFormConfig';
+export { useProjectsFilters } from './useProjectsFilters';
