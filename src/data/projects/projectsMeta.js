795a8c3f73a65c22e1374c2ac877bc6eb25061ac
najijrwan <najijrwanyt@gmail.com>
2026-03-03 00:50:55 +0200

    refactor(data): enhance categories with unique IDs and config object

--- src/data/projects/projectsMeta.js
+++ src/data/projects/projectsMeta.js
@@ -1,19 +1,19 @@
 export const projectsMeta = [
     {
         title: "Fitnessoo",
-        type: "websites",
+        type: "Websites",
         img: "/images/Fitnessoo.png",
         link: "https://fitnessoo.infinityfreeapp.com/index.php"
     },
     {
         title: "Ma7ali",
-        type: "applications",
+        type: "PWA",
         img: "/images/Ma7ali.png",
         link: "https://paperless-a64a0.web.app/"
     },
     {
         title: "Quizo",
-        type: "Desktop Applications",
+        type: "Windows Applications",
         img: "/images/Quizo.png",
         link: "https://github.com/najijrwan/Quizo"
     },
