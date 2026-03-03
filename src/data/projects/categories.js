6908f829bc158181220931e52ecfed02ce541fb1
najijrwan <najijrwanyt@gmail.com>
2026-03-03 04:19:42 +0200

    feat(data): change categories value strings

--- src/data/projects/categories.js
+++ src/data/projects/categories.js
@@ -1,7 +1,7 @@
 export const CATEGORIES = [
     { id: "all", label: "All", value: "all" },
-    { id: "websites", label: "Websites", value: "websites" },
-    { id: "pwa", label: "PWA", value: "pwa" },
-    { id: "windows", label: "Windows Applications", value: "windows applications" },
+    { id: "websites", label: "Websites", value: "website" },
+    { id: "pwa", label: "PWAs", value: "pwa" },
+    { id: "windows", label: "Windows Applications", value: "windows application" },
 ];
 
