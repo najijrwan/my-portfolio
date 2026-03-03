d960581d23545fbf030c02035a1a3552ca0e34a7
najijrwan <najijrwanyt@gmail.com>
2026-03-03 02:50:34 +0200

    refactor(data): change CATEGOREIS back to an array of objects and add id to each category

--- src/data/projects/categories.js
+++ src/data/projects/categories.js
@@ -1,9 +1,7 @@
-export const CATEGORY_CONFIG = {
-  ALL: { id: "all", label: "All", value: "all" },
-  WEBSITES: { id: "websites", label: "Websites", value: "websites" },
-  PWA: { id: "pwa", label: "PWA", value: "pwa" },
-  WINDOWS: { id: "windows", label: "Windows Applications", value: "windows applications" }
-};
-
-export const CATEGORIES = Object.values(CATEGORY_CONFIG).map(c => c.label);
+export const CATEGORIES = [
+    { id: "all", label: "All", value: "all" },
+    { id: "websites", label: "Websites", value: "websites" },
+    { id: "pwa", label: "PWA", value: "pwa" },
+    { id: "windows", label: "Windows Applications", value: "windows applications" },
+];
 
