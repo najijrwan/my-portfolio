eb4ddc084505f4e5dfc1fa6a3f70eabf22505fa2
najijrwan <najijrwanyt@gmail.com>
2026-03-04 21:59:22 +0200

    feat(data): remove projected property and add the scale to gpa property value

--- src/data/resume/educations.js
+++ src/data/resume/educations.js
@@ -3,8 +3,7 @@ export const EDUCATIONS = [
     degree: 'Bachelor of Science in Computer Science',
     uni: 'Lebanese International University',
     uniLocation: 'Lebanon, West Beqaa, Alkhyara',
-    gpa: '3.66',
-    projected: '3.8',
+    gpa: '3.66/4',
     duration: '2022 - 2025',
   },
 ]
