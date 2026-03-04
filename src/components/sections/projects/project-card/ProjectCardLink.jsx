b038cce31236542f500f6d3f8574b6470629b1bb
najijrwan <najijrwanyt@gmail.com>
2026-03-04 04:41:11 +0200

    style(projects): change styles to make the card appear better

--- src/components/sections/projects/project-card/ProjectCardLink.jsx
+++ src/components/sections/projects/project-card/ProjectCardLink.jsx
@@ -2,15 +2,11 @@ const ProjectCardLink = ({ link, iconName }) => (
     <a
         href={link}
         target='_blank'
-        className="
-            p-[14px]
-            flex-center
-            bg-jet rounded-[12px] opacity-0
-            custom-transition-1 scale-[0.8] group-hover:scale-[1.3] group-hover:opacity-100"
+        className="flex-center"
     >
         <ion-icon
             name={iconName}
-            className="size-4 text-yellow-crayola [--ionicon-stroke-width:50px]"
+            className="size-5 text-yellow-crayola [--ionicon-stroke-width:50px]"
         />
     </a>
 )
