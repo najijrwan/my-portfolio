c56885bfdc58c604017853cab8e2067ceed79c31
najijrwan <najijrwanyt@gmail.com>
2026-03-04 19:39:25 +0200

    feat(data): import honors and certificates PDFs and store them in HONORS accordingly

--- src/data/resume/honors.js
+++ src/data/resume/honors.js
@@ -3,18 +3,18 @@ export const HONORS = [
     title: "Dean's List",
     issuer: "LIU",
     date: "June 2023",
-    src: "/cv.pdf",
+    src: "/Naji_Aljarwan_Dean's_List.pdf",
   },
   {
     title: "Presedints List",
     issuer: "LIU",
     date: "June 2024",
-    src: "/cv.pdf",
+    src: "/Naji_Aljarwan_President's_List.pdf",
   },
   {
     title: "7th SAS Students Research Day",
     issuer: "LIU",
     date: "May 2025",
-    src: "/cv.pdf",
+    src: "/Naji_Aljarwan_7th _SAS_Students_Research_Day.pdf",
   }
 ]
