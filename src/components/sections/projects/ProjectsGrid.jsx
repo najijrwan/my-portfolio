fb0c5620604d70fad30c822e8c2ba7e40d190500
najijrwan <najijrwanyt@gmail.com>
2026-03-04 18:52:34 +0200

    fix(proejcts): use project name instead of id for unique react keys and projects filtering

--- src/components/sections/projects/ProjectsGrid.jsx
+++ src/components/sections/projects/ProjectsGrid.jsx
@@ -4,14 +4,14 @@ import ProjectCard from './project-card/ProjectCard'
 const ProjectsGrid = ({ selectedRole }) => (
     <ul className="mb-2.5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7.5">
         {PROJECTS
-            .filter(project => project.id !== "portfolio")
+            .filter(project => project.name !== "portfolio")
             .sort((a, b) => a.rank - b.rank)
             .map((project) => {
                 const isVisible = selectedRole === "all" || project.role.toLowerCase() === selectedRole;
 
                 return (
                     <li
-                        key={project.id}
+                        key={project.name}
                         className={`block w-full ${isVisible ? "animate-scaleUp" : "hidden"}`}
                     >
                         <ProjectCard project={project} />
