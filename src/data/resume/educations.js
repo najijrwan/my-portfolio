b23b0397e584efa7791ea585642e4231b4045b5f
najijrwan <najijrwanyt@gmail.com>
2026-03-04 21:14:17 +0200

    feat(data): add key property 'projected' with value '3.8' to point a future plan

--- src/data/resume/educations.js
+++ src/data/resume/educations.js
@@ -4,6 +4,7 @@ export const EDUCATIONS = [
     uni: 'Lebanese International University',
     uniLocation: 'Lebanon, West Beqaa, Alkhyara',
     gpa: '3.66',
+    projected: '3.8',
     duration: '2022 - 2025',
   },
 ]
