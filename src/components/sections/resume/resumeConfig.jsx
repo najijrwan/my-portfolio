bc6620c0d824a6553c58f1b6f2afe85bca9d356f
najijrwan <najijrwanyt@gmail.com>
2026-03-04 19:23:54 +0200

    feat(resume): filter and sort the projects based on rank and name

--- src/components/sections/resume/resumeConfig.jsx
+++ src/components/sections/resume/resumeConfig.jsx
@@ -36,29 +36,37 @@ export const resumeSections = [
   },
   {
     heading: "PROJECTS",
-    content: PROJECTS.map((item) => (
-      <>
-        <header className="mb-2 text-white">
-          <h1><i className='uppercase'>{item.name}</i> &nbsp; • &nbsp; {item.subTitle}</h1>
-          <h2>stack: <i>{item.stack.join(" - ")}</i></h2>
-        </header>
-        <ul key={item.title}>
-          {item.details?.map((skill) => (
-            <li
-              key={skill}
-              className="
-              relative
-              pl-[1em] ml-5
-              before:content-[''] before:absolute before:left-0 before:top-[9px]
-              before:size-[5px]
-              before:bg-[#ffd700] before:rounded-full"
-            >
-              {skill}
-            </li>
-          ))}
-        </ul>
-      </>
-    )),
+    content: PROJECTS
+      .filter((item) => item.rank <= 3 && item.name !== "portfolio")
+      .sort((a, b) => a.rank - b.rank)
+      .map((item) => (
+        <div key={item.title}>
+          <header className="mb-2 text-white">
+            <h1>
+              <i className="uppercase">{item.name}</i> &nbsp; • &nbsp; {item.subTitle}
+            </h1>
+            <h2>
+              stack: <i>{item.stack.join(" - ")}</i>
+            </h2>
+          </header>
+          <ul>
+            {item.details?.map((skill) => (
+              <li
+                key={skill}
+                className="
+                relative
+                pl-[1em] ml-5
+                before:content-[''] before:absolute before:left-0 before:top-[9px]
+                before:size-[5px]
+                before:bg-[#ffd700] before:rounded-full
+              "
+              >
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )),
   },
   {
     heading: "Education",
