d7a7dfab817327df3ca8063501211d335e31ed4e
najijrwan <najijrwanyt@gmail.com>
2026-03-05 19:26:56 +0200

    refator(data): update educations properties uni and uniLocation values to be all in small letters

--- src/data/resume/educations.js
+++ src/data/resume/educations.js
@@ -1,8 +1,8 @@
 export const EDUCATIONS = [
   {
     degree: 'Bachelor of Science in Computer Science',
-    uni: 'Lebanese International University',
-    uniLocation: 'Lebanon, West Beqaa, Alkhyara',
+    uni: 'lebanese international university',
+    uniLocation: 'lebanon, west Beqaa, alkhyara',
     gpa: '3.66/4',
     duration: '2022 - 2025',
   },
