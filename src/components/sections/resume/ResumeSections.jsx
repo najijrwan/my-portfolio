1e8bc00d207415a144b88b50c7989547a75156a5
najijrwan <najijrwanyt@gmail.com>
2026-03-06 21:40:48 +0200

    refactor(resume): import SectonRenderer and update ResumeSections and SectionContent to correctly render the items (formly content)

--- src/components/sections/resume/ResumeSections.jsx
+++ src/components/sections/resume/ResumeSections.jsx
@@ -1,5 +1,6 @@
-import { IconBox } from '@ui';
-import { resumeSections } from './resumeConfig';
+import { IconBox } from '@ui'
+import { resumeSections } from './resumeConfig'
+import SectionRenderer from './SectionRenderer'
 
 const SectionHeading = ({ heading }) => (
     <header className="mb-5 sm:mb-6 flex items-center gap-[15px]">
@@ -11,22 +12,23 @@ const SectionHeading = ({ heading }) => (
     </header>
 )
 
-const SectionContent = ({ content = [] }) => (
-    <ul className='ml-[45px] sm:ml-[65px] text-[14px] sm:text-[15px]'>
-        {content.map((item, i) => (
+const SectionContent = ({ items }) => (
+    <ul className="ml-[45px] sm:ml-[65px] text-[14px] sm:text-[15px]">
+        {items.map((item, i) => (
             <li
                 key={i}
-                className='
+                className="
                     relative
                     mb-4.5 sm:mb-5
                     text-light-gray text-[15px] font-[300]
-                    before:content-[""] before:absolute before:left-[-30px] sm:before:left-[-41px] 
+                    before:content-[''] before:absolute before:left-[-30px] sm:before:left-[-41px]
                     before:top-[-20px] sm:before:-top-[30px]
                     before:w-px before:h-[calc(100%+20px)] sm:before:h-[calc(100%+24px)]
                     before:bg-jet
-                    after:content-[""] after:absolute after:top-[5px] after:left-[-33px] sm:after:left-[-45px]
+                    after:content-[''] after:absolute after:top-[5px] after:left-[-33px] sm:after:left-[-45px]
                     after:size-[6px] sm:after:size-2
-                    after:bg-gradient-yellow-3 after:rounded-full after:shadow-[0_0_0_4px_var(--color-jet)]'
+                    after:bg-gradient-yellow-3 after:rounded-full after:shadow-[0_0_0_4px_var(--color-jet)]
+                "
             >
                 {item}
             </li>
@@ -34,14 +36,17 @@ const SectionContent = ({ content = [] }) => (
     </ul>
 )
 
-const ResumeSections = () => (
-    resumeSections.map(({ heading, content }) => (
-        <section key={heading} className='mb-6 sm:mb-7.5'>
-            <SectionHeading heading={heading} />
+const ResumeSections = () =>
+    resumeSections.map(({ heading, type, data }) => {
+        const items = SectionRenderer(type, data);
 
-            <SectionContent content={content} />
-        </section >
-    ))
-)
+        return (
+            <section key={heading} className="mb-6 sm:mb-7.5">
+                <SectionHeading heading={heading} />
+
+                <SectionContent items={items} />
+            </section>
+        );
+    });
 
 export default ResumeSections;
