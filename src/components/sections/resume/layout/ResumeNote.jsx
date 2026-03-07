5a9d81cbd193babcfa08574cbb42f4303a9a8f66
najijrwan <najijrwanyt@gmail.com>
2026-03-07 18:45:11 +0200

    feat(resume): change the layout and styles of ResumeNote

--- src/components/sections/resume/layout/ResumeNote.jsx
+++ src/components/sections/resume/layout/ResumeNote.jsx
@@ -1,18 +1,26 @@
 import { VariousActionsButton } from '@ui'
 
 const ResumeNote = () => (
-    <div className="flex items-center gap-2 mb-6 sm:mb-7.5">
-        <ion-icon name="alert-circle-outline" className='text-yellow-500 size-5 sm:size-6 flex-shrink-0' />
+    <div
+        className="
+            mb-7.5 sm:mb-9
+            flex flex-col lg:flex-row items-start lg:items-center lg:justify-between gap-3
+        "
+    >
+        <div className='flex items-center gap-2'>
+            <ion-icon name="alert-circle-outline" className='text-yellow-500 size-5 sm:size-6 flex-shrink-0' />
 
-        <p className="text-yellow-500 text-sm sm:text-base">
-            This is an extended version — download the PDF for a printable summary.
-        </p>
+            <p className="text-yellow-500 text-sm sm:text-base">
+                This is an extended version — download the PDF for a printable summary.
+            </p>
+        </div>
 
         <VariousActionsButton
             onClick={() => window.open('/Naji_Aljarwan_resume.pdf', '_blank')}
             variant='v1'
             iconName='document-text-outline'
             label='Download CV'
+            className='ml-0!'
         />
     </div>
 )
