ddf42d4e2fa4e32f1d6ad507122ae8b5b651c45f
najijrwan <najijrwanyt@gmail.com>
2026-03-07 18:55:28 +0200

    style(ui): change the position of JumpToTopBottom for all screens

--- src/components/ui/buttons/JumpToTopButton.jsx
+++ src/components/ui/buttons/JumpToTopButton.jsx
@@ -13,7 +13,8 @@ const JumpToTopButton = () => {
         <button
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
             className="
-                fixed bottom-22.5 right-19.5
+                fixed bottom-[36px] sm:bottom-[131px] lg:bottom-[91px] right-[33px] sm:right-[91px]
+                md:right-[65px] lg:right-[68px] xl:right-[79px]
                 flex-center
                 text-eerie-2
                 bg-vegas-gold p-3 rounded-xl shadow-lg
