cb27055e6486ebfe8573cc99b27afa1945a1dd82
najijrwan <najijrwanyt@gmail.com>
2026-03-08 03:23:50 +0200

    fix(data): update honors src path

--- src/data/resume/honors.js
+++ src/data/resume/honors.js
@@ -3,21 +3,21 @@ export const HONORS = [
     title: "dean's list",
     issuer: "lebanese international university",
     date: "spring 2023",
-    src: "/Naji_Aljarwan_Dean's_List.pdf",
+    src: "/pdfs/Naji_Aljarwan_Dean's_List.pdf",
     description: "Awarded for outstanding academic achievement in the School of Arts and Sciences",
   },
   {
     title: "presedint's list",
     issuer: "lebanese international university",
     date: "spring 2024",
-    src: "/Naji_Aljarwan_President's_List.pdf",
+    src: "/pdfs/Naji_Aljarwan_President's_List.pdf",
     description: "Recognized for exceptional academic performance with a perfect semester GPA",
   },
   {
     title: "7th SAS students research day",
     issuer: "lebanese international university",
     date: "spring 2025",
-    src: "/Naji_Aljarwan_7th_SAS_Students_Research_Day.pdf",
+    src: "/pdfs/Naji_Aljarwan_7th_SAS_Students_Research_Day.pdf",
     description: "Presented research findings at the School of Arts and Sciences annual research symposium",
   }
 ]
