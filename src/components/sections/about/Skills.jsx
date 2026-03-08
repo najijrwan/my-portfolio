b9615e234f7270c9182e46a400ea7b1a189ba168
najijrwan <najijrwanyt@gmail.com>
2026-03-08 19:42:19 +0200

    fix(about): update  SKILLS_IMAGES name to SKILLS_IMAGE

--- src/components/sections/about/Skills.jsx
+++ src/components/sections/about/Skills.jsx
@@ -1,5 +1,5 @@
 import SectionContainer from './SectionContainer';
-import { SKILLS_IMAGES } from '@data';
+import { SKILLS_IMAGE } from '@data';
 
 const Skills = () => (
     <SectionContainer title='Skills'>
@@ -9,7 +9,7 @@ const Skills = () => (
                 flex-start-start gap-[15px] sm:gap-[50px]
                 has-scrollbar scroll-smooth overscroll-x-contain snap-both scroll-px-6 sm:scroll-px-[45px]"
         >
-            {SKILLS_IMAGES.map((src) => (
+            {SKILLS_IMAGE.map((src) => (
                 <li
                     key={src}
                     className='min-w-[50%] bg-white/10 snap-start rounded-4xl clients-item'
