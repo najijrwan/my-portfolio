178ea405cbefaae9168ffc5adc4230e39f68e644
najijrwan <najijrwanyt@gmail.com>
2026-03-09 02:48:27 +0200

    feat(ui): use and render TechTooltip for StackIcons

--- src/components/ui/StackIcons.jsx
+++ src/components/ui/StackIcons.jsx
@@ -1,13 +1,24 @@
 import { iconMap } from '@utils'
+import { TechTooltip } from './';
 
 const StackIcons = ({ stack, className = '' }) => {
     return (
-        <ul className={`inline-flex items-center gap-4 ${className}`}>
+        <ul className={`inline-flex flex-wrap items-center gap-2 ${className}`}>
             {stack.map((tech) => {
                 const Icon = iconMap[tech];
                 return Icon ? (
-                    <li key={tech} className="flex items-center gap-1 bg-jet/30 rounded-md">
+                    <li
+                        key={tech}
+                        className="
+                            relative group
+                            px-3 py-1
+                            flex items-center
+                            bg-jet/30 rounded-md
+                        "
+                    >
                         <Icon size={20} />
+                        
+                        <TechTooltip tech={tech} />
                     </li>
                 ) : (
                     <li key={tech} className="text-xs bg-jet/30 px-2 py-1 rounded-md">
