5e334ec1226d1bad167865f61146c7ab086068bf
najijrwan <najijrwanyt@gmail.com>
2026-03-09 02:56:38 +0200

    feat(data): add a label (icon name and tech label)  for all techs

--- src/data/about/techs.js
+++ src/data/about/techs.js
@@ -1,50 +1,62 @@
 export const TECHS = [
     {
         tech: 'java',
+        label: 'Java',
         src: 'images/skills-image/java.png',
     },
     {
         tech: 'html',
+        label: 'HTML5',
         src: 'images/skills-image/html.png',
     },
     {
         tech: 'css',
+        label: 'CSS3',
         src: 'images/skills-image/css.png',
     },
     {
         tech: 'javascript',
+        label: 'JavaScript',
         src: 'images/skills-image/javascript.png',
     },
     {
         tech: 'php',
+        label: 'PHP',
         src: 'images/skills-image/php.png',
     },
     {
         tech: 'csharp',
+        label: 'CSharp',
         src: 'images/skills-image/csharp.png',
     },
     {
         tech: 'python',
+        label: 'Python',
         src: 'images/skills-image/python.png',
     },
     {
         tech: 'tailwindcss',
+        label: 'TailwindCSS',
         src: 'images/skills-image/tailwind-css.png',
     },
     {
         tech: 'react',
+        label: 'React',
         src: 'images/skills-image/react.png',
     },
     {
         tech: 'github',
+        label: 'Github',
         src: 'images/skills-image/github.png'
     },
     {
         tech: 'firebase',
+        label: 'Firebase',
         src: 'images/skills-image/firebase.png'
     },
     {
         tech: 'vercel',
+        label: 'VercelLight',
         src: 'images/skills-image/vercel.png'
     },
 ]
