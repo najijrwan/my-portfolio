70a9cb0402472f661f779ee2a707d058fe339305
najijrwan <najijrwanyt@gmail.com>
2026-03-09 19:32:50 +0200

    style(ui): change TechTooltip position styles

--- src/components/sections/about/Skills.jsx
+++ src/components/sections/about/Skills.jsx
@@ -6,7 +6,8 @@ const Skills = () => (
     <SectionContainer title='Skills'>
         <ul
             className="
-                p-[25px] pt-0 sm:pt-0 sm:p-[45px] mx-[-15px] sm:mx-[-30px] overflow-x-auto
+                p-[25px] pt-0 xl:p-[45px] mx-[-15px] sm:mx-[-30px]
+                overflow-x-auto
                 flex-start-start gap-[15px] sm:gap-[50px]
                 has-scrollbar scroll-smooth overscroll-x-contain snap-both scroll-px-6 sm:scroll-px-[45px]"
         >
