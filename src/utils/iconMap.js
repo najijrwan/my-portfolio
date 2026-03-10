f7f3c1c6e8c732d76204c1da011889ce817705e2
najijrwan <najijrwanyt@gmail.com>
2026-03-10 19:12:01 +0200

    fix(utils): update iconMap object to use the values TECHS_BY_TECH

--- src/utils/iconMap.js
+++ src/utils/iconMap.js
@@ -1,8 +1,8 @@
-import { TECHS } from '@data';
+import { TECHS_BY_TECH } from '@data';
 import * as DevIcons from "developer-icons";
 
 export const iconMap = Object.fromEntries(
-    TECHS.map(tech => [
+    Object.values(TECHS_BY_TECH).map(tech => [
         tech.tech,
         DevIcons[tech.label || tech.name]
     ])
