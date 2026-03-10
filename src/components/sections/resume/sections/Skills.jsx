e94d38986ea646979c5951283761cb0c48b72572
najijrwan <najijrwanyt@gmail.com>
2026-03-10 19:13:48 +0200

    refactor(resume): normalise and seperate components for Skills secton in resume

--- src/components/sections/resume/sections/Skills.jsx
+++ src/components/sections/resume/sections/Skills.jsx
@@ -1,104 +1,74 @@
-const Skills = ({ category }) => (
-    <>
-        <header className="mb-2 text-white">
-            {category.category}
-        </header>
-
-        {category.type === 'proficiency' && (
-            <>
-                {category.proficient?.length > 0 && (
-                    <>
-                        <span className="text-vegas-gold text-sm block mb-1 ml-5">
-                            Proficient in:
-                        </span>
-                        <ul className="mb-3">
-                            {category.proficient.map((skill) => (
-                                <li
-                                    key={skill}
-                                    className="
-                        relative
-                        pl-[1em] ml-10
-                        before:content-[''] before:absolute before:left-0 before:top-[9px]
-                        before:size-[5px]
-                        before:bg-[#ffd700] before:rounded-full
-                      "
-                                >
-                                    {skill}
-                                </li>
-                            ))}
-                        </ul>
-                    </>
-                )}
-
-                {category.experienced?.length > 0 && (
-                    <>
-                        <span className="text-vegas-gold text-sm block mb-1 ml-5">
-                            Experienced with:
-                        </span>
-                        <ul className="mb-3">
-                            {category.experienced.map((skill) => (
-                                <li
-                                    key={skill}
-                                    className="
-                      relative
-                      pl-[1em] ml-10
-                      before:content-[''] before:absolute before:left-0 before:top-[9px]
-                      before:size-[5px]
-                      before:bg-[#ffd700] before:rounded-full
-                    "
-                                >
-                                    {skill}
-                                </li>
-                            ))}
-                        </ul>
-                    </>
-                )}
-
-                {category.familiar?.length > 0 && (
-                    <>
-                        <span className="text-vegas-gold text-sm block mb-1 ml-5">
-                            Familiar with:
-                        </span>
-                        <ul className="mb-3">
-                            {category.familiar.map((skill) => (
-                                <li
-                                    key={skill}
-                                    className="
-                      relative
-                      pl-[1em] ml-10
-                      before:content-[''] before:absolute before:left-0 before:top-[9px]
-                      before:size-[5px]
-                      before:bg-[#ffd700] before:rounded-full
-                    "
-                                >
-                                    {skill}
-                                </li>
-                            ))}
-                        </ul>
-                    </>
-                )}
-            </>
-        )}
-
-        {category.type === 'list' && (
-            <ul>
-                {category.items.map((item) => (
+const ProficiencySection = ({ title, skills }) => (
+    skills?.length > 0 && (
+        <>
+            <span className="text-vegas-gold text-sm block mb-1 ml-5">
+                {title}:
+            </span>
+            <ul className="mb-3">
+                {skills.map((skill) => (
                     <li
-                        key={item}
+                        key={skill.name}
                         className="
-                relative
-                pl-[1em] ml-5
-                before:content-[''] before:absolute before:left-0 before:top-[9px]
-                before:size-[5px]
-                before:bg-[#ffd700] before:rounded-full
-              "
+                            relative
+                            pl-[1em] ml-10
+                            before:content-[''] before:absolute before:left-0 before:top-[9px]
+                            before:size-[5px] before:bg-[#ffd700] before:rounded-full
+                        "
                     >
-                        {item}
+                        {skill.name}
                     </li>
                 ))}
             </ul>
-        )}
-    </>
+        </>
+    )
 );
+
+const ListSection = ({ items }) => (
+    <ul>
+        {items.map((item) => (
+            <li
+                key={item}
+                className="
+                    relative
+                    pl-[1em] ml-5
+                    before:content-[''] before:absolute before:left-0 before:top-[9px]
+                    before:size-[5px] before:bg-[#ffd700] before:rounded-full
+                "
+            >
+                {item}
+            </li>
+        ))}
+    </ul>
+);
+
+const Skills = ({ category }) => {
+    const proficiencyMap = {
+        proficient: 'Proficient in',
+        experienced: 'Experienced with',
+        familiar: 'Familiar with'
+    };
+
+    return (
+        <>
+            <header className="mb-2 text-white">
+                {category.category}
+            </header>
+
+            {category.type === 'proficiency' ? (
+                // Render proficiency categories
+                Object.entries(proficiencyMap).map(([key, title]) => (
+                    <ProficiencySection
+                        key={key}
+                        title={title}
+                        skills={category[key]}
+                    />
+                ))
+            ) : (
+                // Render list type (tools or key strengths)
+                <ListSection items={category.items || category.tools?.map(t => t.name)} />
+            )}
+        </>
+    );
+};
 
 export default Skills;
