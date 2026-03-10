29e1a07f712622e3527a6be2be9b7d16c2a44296
najijrwan <najijrwanyt@gmail.com>
2026-03-10 21:25:19 +0200

    style(ui): update styles for TechTooltip and add a before pseudo element

--- src/components/ui/TechTooltip.jsx
+++ src/components/ui/TechTooltip.jsx
@@ -1,11 +1,19 @@
 const TechTooltip = ({ tech }) => (
     <span
         className='
-            absolute left-1/2 -translate-x-1/2 -top-10 z-20
+            absolute left-1/2 -translate-x-1/2 -top-11 z-20
             min-w-max px-2 py-1
             opacity-0
             bg-jet text-yellow-crayola rounded-lg
             group-hover:opacity-100 custom-transition-1 cursor-default
+            shadow-1
+
+            before:content-[""]
+            before:absolute before:left-1/2 before:-translate-x-1/2 before:bottom-0 before:translate-y-full
+            before:border-l-[7px] before:border-l-transparent
+            before:border-b-[6px] before:border-b-transparent
+            before:border-r-[7px] before:border-r-transparent
+            before:border-t-[6px] before:border-t-jet
         '
     >
         {tech}
