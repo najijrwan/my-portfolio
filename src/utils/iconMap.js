7402aa659f01578cb624a3d879d62b9da404895c
najijrwan <najijrwanyt@gmail.com>
2026-03-10 19:29:14 +0200

    refactor(utils): use skill instead of tech as the mapping property and use skill.name as the key property

--- src/utils/iconMap.js
+++ src/utils/iconMap.js
@@ -2,8 +2,8 @@ import { TECHS_BY_TECH } from '@data';
 import * as DevIcons from "developer-icons";
 
 export const iconMap = Object.fromEntries(
-    Object.values(TECHS_BY_TECH).map(tech => [
-        tech.tech,
-        DevIcons[tech.label || tech.name]
+    Object.values(TECHS_BY_TECH).map(skill => [
+        skill.name,
+        DevIcons[skill.label || skill.name]
     ])
 );
