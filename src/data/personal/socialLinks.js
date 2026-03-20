7eabe55c3d4038fd49e351eabbe135c165285458
najijrwan <najijrwanyt@gmail.com>
2026-03-20 09:21:50 +0200

    feat(data): update social link leetcode link

--- src/data/personal/socialLinks.js
+++ src/data/personal/socialLinks.js
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
     linkedin: 'https://www.linkedin.com/in/najialjarwan',
     github: 'https://github.com/najialjarwan',
-    leetcode: 'https://leetcode.com/u/NajiJrwan',
+    leetcode: 'https://leetcode.com/u/najialjarwan',
     // telegram: 'https://t.me/+96170504841',
 }
